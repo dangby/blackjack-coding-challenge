2e04fabff01ffb8ffaf91fad4ae49918aca9244c
dangby <37348853+dangby@users.noreply.github.com>
2019-01-04 08:27:08 -0700

    change for intended OOP implementation

--- danielgibby/blackjack/ParticipantDealer.java
+++ danielgibby/blackjack/ParticipantDealer.java
@@ -2,6 +2,8 @@ package danielgibby.blackjack;
 
 public class ParticipantDealer extends Participant {
 
+	private int maxNoBustPlayerTotal;
+
 	public ParticipantDealer() {
 		super();
 		super.makeDealer();
@@ -12,4 +14,46 @@ public class ParticipantDealer extends Participant {
 		this.setId(id);
 	}
 	
+	/**
+	 * ParticipantPlayer asks for player input
+	 *  implements abstract Participant method
+	 */
+	@Override
+	public void hitOrStay() {
+		forceDealerHand();
+	}
+	
+	/**
+	 * force Hit/Stay according to rules for Dealer
+	 */
+	protected void forceDealerHand() {
+
+		int dealerTotal = this.getCardTotal();
+		Hand dealerHand = this.getActiveHand();
+		if (HandPlayState.BlackJack.equals(dealerHand.getHandPlayState())
+				|| HandPlayState.Bust.equals(dealerHand.getHandPlayState())
+				|| HandPlayState.Stay.equals(dealerHand.getHandPlayState())) {
+			// do nothing
+		} else {
+			if (dealerTotal < 17) {
+				dealerHand.hit();
+			} else if (dealerHand.isHasHighAce() && dealerTotal < 18) {
+				dealerHand.hit();
+			} else if (maxNoBustPlayerTotal > dealerTotal) {
+				dealerHand.hit();
+			} else {
+				dealerHand.stay();
+			}
+		}
+	}
+
+	protected int getMaxNoBustPlayerTotal() {
+		return maxNoBustPlayerTotal;
+	}
+
+	protected void setMaxNoBustPlayerTotal(int maxNoBustPlayerTotal) {
+		this.maxNoBustPlayerTotal = maxNoBustPlayerTotal;
+	}
+
+
 }
