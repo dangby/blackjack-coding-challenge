92a196a1474f57a0b13cc40067b96f3984c26ea8
dangby <37348853+dangby@users.noreply.github.com>
2019-01-04 08:27:37 -0700

    change for intended OOP implementation

--- danielgibby/blackjack/ParticipantPlayer.java
+++ danielgibby/blackjack/ParticipantPlayer.java
@@ -19,4 +19,39 @@ public class ParticipantPlayer extends Participant {
 		this.setId(id);
 	}
 	
+	/**
+	 * ParticipantPlayer asks for player input -
+	 *  implements abstract Participant method
+	 */
+	public void hitOrStay() {
+		getPlayerInput();
+	}
+	
+	
+	/**
+	 * for each Hand: when not already Bust or BlackJack, get Player input or force
+	 * Dealer Hit or Stay
+	 */
+	protected void getPlayerInput() {
+
+		// get input from each Player for each Hand
+		for (Hand hand : this.getHands()) {
+
+			// ask if they want Hit or Stay for this Hand
+			if (HandPlayState.BlackJack.equals(hand.getHandPlayState())
+					|| HandPlayState.Bust.equals(hand.getHandPlayState())
+					|| HandPlayState.Stay.equals(hand.getHandPlayState())) {
+			} else {
+				String input = "Hit";
+				// TODO: wait for input of Hit/Stay here
+				if ("Hit".equals(input)) {
+					hand.hit();
+				} else if ("Stay".equals(input)) {
+					hand.stay();
+				}
+			}
+		}
+	}
+	
+	
 }
